4b829e773ad2f0af80e62033a766b16a95c09370
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-01-09 20:27:26 +0530

    done with rendering and predictions

--- components/object-detection.js
+++ components/object-detection.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import {load as cocoSSDload} from "@tensorflow-models/coco-ssd";
 import * as tf from '@tensorflow/tfjs'
+import { renderPredictions } from '@/utils/render-predictions';
 
 
 let detectInterval;
@@ -12,6 +13,7 @@ const ObjectDetection = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     const webcamRef = useRef(null);
+    const canvasRef = useRef(null);
 
     const runCoco = async () => {
         setIsLoading(true);
@@ -20,10 +22,30 @@ const ObjectDetection = () => {
 
 
         detectInterval = setInterval(()=> {
-            // runObjectDetection(net);
+            runObjectDetection(net);
         },10)
 
     };
+
+    async function runObjectDetection(net) {
+        if(
+            canvasRef.current &&
+            webcamRef.current !== null &&
+            webcamRef.current.video?.readyState ===4
+        ){
+            canvasRef.current.width = webcamRef.current.video.videoWidth;
+            canvasRef.current.height = webcamRef.current.video.videoHeight;
+
+            // find detected objects
+            const detectedObjects = await net.detect(webcamRef.current.video,undefined,0.6);
+            // console.log(detectedObjects);
+            
+            const context = canvasRef.current.getContext("2d");
+            renderPredictions(detectedObjects,context);
+
+
+        }        
+    }
 
     const showVideo = () => {
         if (
@@ -58,6 +80,13 @@ const ObjectDetection = () => {
                 />
 
                 {/* Canvas (if needed later) */}
+                <canvas ref={canvasRef} 
+                className="absolute top-0 left-0 z-99999 w-full lg:h-[720px]"
+                />
+
+
+
+
             </div>}
         </div>
     );
